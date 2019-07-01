1b0907d74b97d7ccb07da11a6470e66d1cac361b
bryce huang <cherbini@qq.com>
2019-07-01 12:30:38 +0800

    增加mybatis-plus 配种

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 The simplest springcloud solution that meets production requirements, continuous upgrade. . .
 
 Contains springcloud, javaEE, distributed locks, database operations, distributed storage, etc.
+
+## 
+
+gateway：spring cloud gateway
+
+Link tracking: skyWaking
+
+orm: mybatis-plus
+
--- conf/src/main/java/fun/bryce/conf/MybatisPlusConfig.java
+++ conf/src/main/java/fun/bryce/conf/MybatisPlusConfig.java
@@ -1,10 +1,25 @@
 package fun.bryce.conf;
 
+import com.baomidou.mybatisplus.core.parser.ISqlParser;
+import com.baomidou.mybatisplus.core.parser.ISqlParserFilter;
+import com.baomidou.mybatisplus.core.parser.SqlParserHelper;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.tenant.TenantHandler;
+import com.baomidou.mybatisplus.extension.plugins.tenant.TenantSqlParser;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.LongValue;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author bryce
@@ -16,12 +31,78 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class MybatisPlusConfig
 {
 
+    //    /**
+//     * 分页插件
+//     */
+//    @Bean
+//    public PaginationInterceptor paginationInterceptor()
+//    {
+//        return new PaginationInterceptor();
+//    }
+
+
+    // 乐观锁插件
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor()
+    {
+        return new OptimisticLockerInterceptor();
+    }
+
     /**
-     * 分页插件
+     * SQL执行效率插件
      */
+    @Bean
+    @Profile({"dev", "test"})// 设置 dev test 环境开启
+    public PerformanceInterceptor performanceInterceptor()
+    {
+        return new PerformanceInterceptor();
+    }
+
+
     @Bean
     public PaginationInterceptor paginationInterceptor()
     {
-        return new PaginationInterceptor();
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        /*
+         * 【测试多租户】 SQL 解析处理拦截器<br>
+         * 这里固定写成住户 1 实际情况你可以从cookie读取，因此数据看不到 【 麻花藤 】 这条记录（ 注意观察 SQL ）<br>
+         */
+        List<ISqlParser> sqlParserList = new ArrayList<>();
+        TenantSqlParser tenantSqlParser = new TenantSqlParser();
+        tenantSqlParser.setTenantHandler(new TenantHandler()
+        {
+            @Override
+            public Expression getTenantId()
+            {
+                return new LongValue(1L);
+            }
+
+            @Override
+            public String getTenantIdColumn()
+            {
+                return "tenant_id";
+            }
+
+            @Override
+            public boolean doTableFilter(String tableName)
+            {
+                // 这里可以判断是否过滤表
+            /*
+            if ("user".equals(tableName)) {
+                return true;
+            }*/
+                return false;
+            }
+        });
+        sqlParserList.add(tenantSqlParser);
+        paginationInterceptor.setSqlParserList(sqlParserList);
+        paginationInterceptor.setSqlParserFilter(metaObject ->
+        {
+            MappedStatement ms = SqlParserHelper.getMappedStatement(metaObject);
+            // 过滤自定义查询此时无租户信息约束【 麻花藤 】出现
+            return "com.baomidou.springboot.mapper.UserMapper.selectListBySQL".equals(ms.getId());
+        });
+        return paginationInterceptor;
     }
+
 }

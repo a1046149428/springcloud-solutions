3b83d0e6675af775eb6f2be243a5f7c7402716fc
cherbini <cherbini@qq.com>
2019-07-04 16:45:58 +0800

    添加elk日志集成方案

--- log4j2/src/main/java/fun/bryce/log4j2/Log4j2Application.java
+++ log4j2/src/main/java/fun/bryce/log4j2/Log4j2Application.java
@@ -6,15 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Slf4j
+
 public class Log4j2Application
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws NoSuchFieldException
     {
         SpringApplication app = new SpringApplication(Log4j2Application.class);
         app.addListeners( new ApplicationStartedEventListener());
         app.run(args);
+        log.info("正常日志》》》》》》》》》》》》");
+        log.info(Log.builder().user("bryce").pwd("www").index("index").build().toString());
 
+        throw new NoSuchFieldException("》》》》》》》》》》》》》》》这是异常日志");
     }
 
 }
